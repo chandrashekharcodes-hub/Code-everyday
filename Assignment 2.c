e30553f6ad6904237e6b526757dad625e2c9dc91
Chandrashekhar Girhepunje <chandugirhepunje000@gmail.com>
2025-04-25 21:22:21 +0530

    Add files via upload

---
+++ Assignment 2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+
+   //char ch;
+   //printf("Enter a character : ");
+   //scanf("%c",&ch);
+   //printf("Ascii code is %d ",ch);
+
+   //int ch;
+   //printf("Enter Ascii code : ");
+  // scanf("%d",&ch);
+   //printf("Ascii character is %c",ch);
+
+   int  d, m , y;
+   printf("Enter date  : ");
+   scanf("%d/%d/%d ",&d,&m,&y);
+   printf("%d/%d/%d",d,m,y);
+
+
+}
