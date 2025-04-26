65c05150efcfb2f829c22a789ec70c8fb6114cfc
Chandrashekhar Girhepunje <chandugirhepunje000@gmail.com>
2025-04-26 20:37:35 +0530

    Add files via upload

---
+++ addition of two number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter value of a and b");
+    scanf("%d %d", &a,&b);
+    c=a+b;
+    printf("sum of %d and %d is %d",a,b,c);
+    getch();
+}
